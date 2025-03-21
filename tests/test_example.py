c90df241a1e4a731399342f62770b0fc649b7d1e
pilgrim-nord <dmelekhov@gmail.com>
2025-03-21 13:45:25 +0300

    add external file test

--- tests/test_example.py
+++ tests/test_example.py
@@ -7,3 +7,15 @@ def test_reverse():
 
 def test_reverse_for_empty_string():
     assert reverse('') == ''
+
+def test_extra():
+    input_file = 'test_data/input_data.txt'
+    output_file = 'test_data/output_data.txt'
+    with open(input_file) as f:
+        input = f.read()
+    with open(output_file) as f:
+        output = f.read()
+    print(input)
+    print(reverse(input))
+    print(output)
+    assert reverse(input) == output
